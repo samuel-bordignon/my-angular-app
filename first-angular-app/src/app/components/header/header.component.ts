13255dbe5349ae677e2a89b1f8c8313b2c252311
samuel-bordignon <samuelborwie@gmail.com>
2026-04-22 09:21:49 -0300

    Criando um componente sem CLI

---
+++ first-angular-app/src/app/components/header/header.component.ts
@@ -0,0 +1,8 @@
+import { Component, input, Input } from "@angular/core";
+
+@Component({
+    selector: "app-header",
+    templateUrl: "./header.component.html",
+    styleUrl: "./header.component.css"
+})
+export class HeaderComponent {}
