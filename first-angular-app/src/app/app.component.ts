d2686616d27d22ef33b430a2610f4588eaf238a7
samuel-bordignon <samuelborwie@gmail.com>
2026-04-22 08:23:32 -0300

    Iniciando primeiro app em angular

---
+++ first-angular-app/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, HeaderComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class App {
+  protected readonly title = signal('first-angular-app');
+}
