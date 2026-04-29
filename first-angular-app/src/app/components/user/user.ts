ea92e780d3cda5c40711287740fb7fc0a8e9be64
samuel-bordignon <samuelborwie@gmail.com>
2026-04-29 17:37:23 -0300

    Botão aleatório de lista de usuários

--- first-angular-app/src/app/components/user/user.ts
+++ first-angular-app/src/app/components/user/user.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
 
+import { DUMMY_USERS } from '../../../data/dummy-users';
+
+let ramdomID = Math.floor(Math.random() * DUMMY_USERS.length)
+
 @Component({
   selector: 'app-user',
   standalone: true,
   templateUrl: './user.html',
   styleUrl: './user.css',
 })
-export class User {}
+export class UserComponent {
+  selectedUser = DUMMY_USERS[ramdomID];
+  get imagePath() {
+    return `assets/users/${this.selectedUser.avatar}`;
+  }
+  onselectUser() {
+    const randomID = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.selectedUser = DUMMY_USERS[randomID];
+  }
+}
