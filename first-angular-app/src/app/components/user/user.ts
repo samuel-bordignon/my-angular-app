9d408f3e1319af4cbf063bf0a38f12f94174a69c
samuel-bordignon <samuelborwie@gmail.com>
2026-04-29 18:19:04 -0300

    Teste com o signal com bananas

--- first-angular-app/src/app/components/user/user.ts
+++ first-angular-app/src/app/components/user/user.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 
 import { DUMMY_USERS } from '../../../data/dummy-users';
 
-let ramdomID = Math.floor(Math.random() * DUMMY_USERS.length)
+const ramdomID = Math.floor(Math.random() * DUMMY_USERS.length)
+const bananas = ["banana1","banana2","banana3","banana4","banana5","banana6"]
 
 @Component({
   selector: 'app-user',
@@ -11,12 +12,18 @@ let ramdomID = Math.floor(Math.random() * DUMMY_USERS.length)
   styleUrl: './user.css',
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[ramdomID];
-  get imagePath() {
-    return `assets/users/${this.selectedUser.avatar}`;
+  selectedUser = signal(DUMMY_USERS[ramdomID]);
+  banana = signal(bananas[ramdomID])
+  imagePath = computed(()=>"assets/users/" + this.selectedUser().avatar)
+  // get imagePath() {
+  //   return `assets/users/${this.selectedUser.get("")avatar}`;
+  // }
+  ramdomBananas(){
+      const randomID = Math.floor(Math.random() * DUMMY_USERS.length);
+      this.banana.set(bananas[randomID]);
   }
   onselectUser() {
     const randomID = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser = DUMMY_USERS[randomID];
+    this.selectedUser.set(DUMMY_USERS[randomID]);
   }
 }
