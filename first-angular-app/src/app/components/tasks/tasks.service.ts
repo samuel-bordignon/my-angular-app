2b8606e5c1e0f02da9e0551de5b068c2cf96eb6c
samue-bordignon <samuelborwie@gmail.com>
2026-06-06 17:23:06 -0300

    finaizando crud de tasks com storage no navegador

--- first-angular-app/src/app/components/tasks/tasks.service.ts
+++ first-angular-app/src/app/components/tasks/tasks.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
 import { DUMMY_TASKS } from "../../../data/dummy-tasks";
-import { CreateTask } from "./tasks.model";
+import { CreateTask, Task } from "./tasks.model";
 
 
 @Injectable({ providedIn: "root" })
@@ -15,20 +15,24 @@ export class TaskService {
     if (response) {
       this.tasks = JSON.parse(response)
     }
-    this.tasks = DUMMY_TASKS
   }
 
   addNewTask(data: CreateTask, userId: string) {
     console.log(data)
     this.tasks.push({
       ...data,
-      id: "t",
+      id: "t" + this.tasks.length,
       userId: userId
     })
+    this.saveTask()
   }
 
   deleteTask(taskId: string) {
     this.tasks = this.tasks.filter(task => task.id !== taskId)
+    this.saveTask()
   }
-  
+
+  saveTask() {
+    localStorage.setItem("tasks", JSON.stringify(this.tasks))
+  }
 }
