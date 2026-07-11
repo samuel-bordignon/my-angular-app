4bdba277e3213ad110f6e549345d5222faeaa2b3
samuel-bordignon <samuelborwie@gmail.com>
2026-07-11 17:10:40 -0300

    adicionando módulo de tarefas e removendo componentes do módulo principal

--- first-angular-app/src/app/app.module.ts
+++ first-angular-app/src/app/app.module.ts
@@ -5,12 +5,8 @@ import { AppComponent } from "./app.component";
 
 import { HeaderComponent } from "./components/header/header.component";
 import { UserComponent } from "./components/user/user.component";
-import { TasksComponent } from "./components/tasks/tasks.component";
-import { CardComponent } from "./components/ui/card/card.component";
-import { TaskComponent } from "./components/tasks/task/task";
-import { NewTaskComponent } from "./components/tasks/new-task/new-task.component";
-import { FormsModule } from "@angular/forms";
 import { UiModule } from "./components/ui/card/ui.module";
+import { TasksModule } from "./components/tasks/tasks.module";
 
 
 @NgModule({
@@ -18,12 +14,9 @@ import { UiModule } from "./components/ui/card/ui.module";
     AppComponent,
     HeaderComponent,
     UserComponent,
-    TasksComponent,
-    TaskComponent,
-    NewTaskComponent
   ],
   bootstrap: [AppComponent],
-  imports: [BrowserModule, RouterOutlet, FormsModule, UiModule]
+  imports: [BrowserModule, RouterOutlet, UiModule, TasksModule]
 })
 export class AppModule {
 
