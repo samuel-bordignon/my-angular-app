bbc328ef568eb4c217cbb97071f19dc6b7845253
samuel-bordignon <samuelborwie@gmail.com>
2026-07-14 21:32:50 -0300

    Funcionalidade: implementação do componente de calculadora com campos de entrada e estilos.

--- investment-calculator/src/app/calculator/calculator.component.ts
+++ investment-calculator/src/app/calculator/calculator.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { InputComponent } from '../ui/input/input.component';
 
 @Component({
   selector: 'app-calculator',
   standalone: true,
-  imports: [],
+  imports: [InputComponent],
   templateUrl: './calculator.component.html',
   styleUrl: './calculator.component.css'
 })
--- investment-calculator/src/app/ui/input/input.component.ts
+++ investment-calculator/src/app/ui/input/input.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-input',
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './input.component.css'
 })
 export class InputComponent {
-
+  @Input({ required: true }) label: string = ""
 }
