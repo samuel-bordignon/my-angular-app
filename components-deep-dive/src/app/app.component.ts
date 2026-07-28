8b6c2c4e0ee10166e29c46931ffbbee30c4bd236
samuel-bordignon <samuelborwie@gmail.com>
2026-07-27 21:16:30 -0300

    Funcionalidade: refatoração do componente de cabeçalho e adição de componente de botão

--- components-deep-dive/src/app/app.component.ts
+++ components-deep-dive/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
-import { DashboardStatusServerComponent } from './dashboard-card/dashboard-status-server/dashboard-status-server.component';
-import { DashboardTrafficComponent } from './dashboard-card/dashboard-traffic/dashboard-traffic.component';
-import { DashboardSuportComponent } from './dashboard-card/dashboard-suport/dashboard-suport.component';
+import { DashboardStatusServerComponent } from './dashboard/dashboard-status-server/dashboard-status-server.component';
+import { DashboardTrafficComponent } from './dashboard/dashboard-traffic/dashboard-traffic.component';
+import { DashboardSuportComponent } from './dashboard/dashboard-suport/dashboard-suport.component';
 import { HeaderComponent } from './header/header.component';
 
 @Component({
--- components-deep-dive/src/app/header/header.component.ts
+++ components-deep-dive/src/app/header/header.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../ui/button/button.component';
 
 @Component({
-  selector: 'app-header',
+  selector: 'header[appHeader]',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
